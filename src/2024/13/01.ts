13b29ec80b15a10863865893169061f4b73211a3
Ray Knight <array.knight+github@gmail.com>
2024-12-13 19:20:00 -0600

    docs: ✏️ math

--- src/2024/13/01.ts
+++ src/2024/13/01.ts
@@ -15,16 +15,36 @@ export default (games: Game[]) => {
 	let total = 0;
 
 	for (const { a, b, prize } of games) {
-		const cA =
-			(b[1] * prize[0] - b[0] * prize[1]) / (b[1] * a[0] - b[0] * a[1]);
+		const [aX, aY] = a;
+		const [bX, bY] = b;
+		const [pX, pY] = prize;
+		/**
+		 * Find intersection of two slopes
+		 *
+		 * Start with defining slopes:
+		 * aX * aCount + bX * bCount = pX
+		 * aY * aCount + bY * bCount = pY
+		 *
+		 * Solve for aCount:
+		 * bCount = (pX - aX * aCount) / bX
+		 * bCount = (pY - aY * aCount) / bY
+		 *
+		 * (pX - aX * aCount) / bX = (pY - aY * aCount) / bY
+		 * bY * (pX - aX * aCount) = bX * (pY - aY * aCount)
+		 * bY * pX - bY * aX * aCount = bX * pY - bX * aY * aCount
+		 * bY * pX - bX * pY = bY * aX * aCount - bX * aY * aCount
+		 * (bY * pX - bX * pY) / (bY * aX - bX * aY) = aCount
+		 */
+		const aCount = (bY * pX - bX * pY) / (bY * aX - bX * aY);
 
-		if (Math.round(cA) !== cA) {
+		// Solution can't involve partial button presses
+		if (!Number.isInteger(aCount)) {
 			continue;
 		}
 
-		const cB = (a[0] * cA - prize[0]) / (b[0] * -1);
+		const bCount = (pX - aX * aCount) / bX;
 
-		total += COST.a * cA + COST.b * cB;
+		total += COST.a * aCount + COST.b * bCount;
 	}
 
 	return total;
