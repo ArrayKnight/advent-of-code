d3d610bd808f7f6fd98b059d112ef751cb1e3c88
Ray Knight <array.knight+github@gmail.com>
2024-12-18 15:51:39 -0800

    perf: ⚡️ simplify, don't track values just visited

--- src/2024/18/01.ts
+++ src/2024/18/01.ts
@@ -1,9 +1,8 @@
-import type { CardinalDirection, Grid, Position } from "../../types";
+import type { Grid, Position } from "../../types";
 import { GridUtils, PositionUtils } from "../../utils";
 
 type Path = {
 	position: Position;
-	positions: Record<string, number>;
 	steps: number;
 };
 
@@ -11,78 +10,51 @@ export default (blocks: Position[], size: Position, count: number) => {
 	const [height, width] = size;
 	const start: Position = [0, 0];
 	const end = PositionUtils.sub(size, [1, 1]);
-	const grid: Grid<number | string> = Array.from({ length: height }, () =>
-		new Array(width).fill(Number.POSITIVE_INFINITY),
+	const grid: Grid<string> = Array.from({ length: height }, () =>
+		new Array(width).fill("."),
 	);
 
 	for (let i = 0; i < count; i++) {
 		GridUtils.set(grid, blocks[i], "#");
 	}
 
-	const paths: Record<CardinalDirection, Path[]> = {
-		N: [
-			{
-				position: start,
-				positions: { [PositionUtils.toString(start)]: 0 },
-				steps: 0,
-			},
-		],
-		E: [],
-		S: [],
-		W: [],
-	};
-	let length = height * width - count;
+	const paths: Path[] = [
+		{
+			position: start,
+			steps: 0,
+		},
+	];
+	const visited = new Set<string>();
 
-	while (paths.N.length || paths.E.length || paths.S.length || paths.W.length) {
-		let path: Path | undefined;
+	while (paths.length) {
+		const path = paths.shift();
 
-		if (!path && paths.E.length) path = paths.E.pop();
-		if (!path && paths.S.length) path = paths.S.pop();
-		if (!path && paths.N.length) path = paths.N.pop();
-		if (!path && paths.W.length) path = paths.W.pop();
 		if (!path) return;
 
 		const { position, steps } = path;
 
-		if (steps >= length) {
+		if (visited.has(PositionUtils.toString(position))) {
 			continue;
 		}
+
+		visited.add(PositionUtils.toString(position));
 
 		if (PositionUtils.equals(position, end)) {
-			length = Math.min(length, steps);
-
-			continue;
+			return steps;
 		}
-
-		const value = GridUtils.get(grid, position);
-
-		if (typeof value === "number" && value < steps) {
-			continue;
-		}
-
-		GridUtils.set(grid, position, steps);
 
 		const adjacent = GridUtils.adjacent(grid, position);
-		const directions = Object.entries(adjacent);
-		const positions = {
-			...path.positions,
-			[PositionUtils.toString(position)]: steps + 1,
-		};
+		const directions = Object.values(adjacent);
 
-		for (const [key, D] of directions) {
-			if (
-				typeof D.value === "number" &&
-				D.value > steps + 1 &&
-				!positions[PositionUtils.toString(D.position)]
-			) {
-				paths[key as CardinalDirection].push({
+		for (const D of directions) {
+			if (D.value && D.value !== "#") {
+				paths.push({
 					position: D.position,
-					positions,
 					steps: steps + 1,
 				});
 			}
 		}
 	}
 
-	return length;
+	return Number.POSITIVE_INFINITY;
 };
