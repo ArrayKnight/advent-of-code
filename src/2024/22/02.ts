ddaad22808145bf31f38960da0911f2640f6f193
Ray Knight <array.knight+github@gmail.com>
2024-12-22 12:28:37 -0800

    refactor: 💡 simplify, reduce calculations

--- src/2024/22/02.ts
+++ src/2024/22/02.ts
@@ -1,42 +1,30 @@
 import { next } from "./01";
 
-function last(int: bigint) {
-	return int % 10n;
-}
-
-function diff(a: bigint, b: bigint) {
-	return a - b;
-}
-
 export default (secrets: bigint[]) => {
-	const latest = new Map<bigint, bigint>(
-		secrets.map((secret) => [secret, secret]),
-	);
-	const diffs = new Map<bigint, bigint[]>(
-		secrets.map((secret) => [secret, []]),
-	);
 	const totals = new Map<string, bigint>();
 	const indexOf = new Map<string, number>();
 	let best = BigInt(Number.MIN_SAFE_INTEGER);
 
 	for (const secret of secrets) {
+		let previous = secret;
+		let prev = previous % 10n;
+		const diffs: bigint[] = [];
+
 		for (let i = 0; i < 2000; i++) {
-			const latestDiffs = diffs.get(secret) ?? [];
-			const previous = latest.get(secret) ?? secret;
-			const prev = last(previous);
 			const current = next(previous);
-			const curr = last(current);
+			const curr = current % 10n;
 
-			latest.set(secret, current);
-			latestDiffs.push(diff(curr, prev));
+			diffs.push(curr - prev);
+			previous = current;
+			prev = curr;
 
-			if (latestDiffs.length < 4) continue;
+			if (diffs.length < 4) continue;
 
-			if (latestDiffs.length > 4) {
-				latestDiffs.shift();
+			if (diffs.length > 4) {
+				diffs.shift();
 			}
 
-			const totalKey = latestDiffs.join();
+			const totalKey = diffs.join();
 			const indexKey = `${totalKey}:${secret}`;
 
 			if (indexOf.has(indexKey)) continue;
